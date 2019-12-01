1bff542384f699b922e2b7600b50c0b9c34de37a
Noland Seigler <nolandseigler@gmail.com>
2019-11-30 18:58:39 -0600

    completed javascript with html except the bottom portion of #3

---
+++ js/external.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("Hello from external JavaScript.");
+alert("Welcome to my Website!");
+var userPromptInput = prompt("What is your favorite color?");
+alert(`${userPromptInput} is my favorite color too!`);
