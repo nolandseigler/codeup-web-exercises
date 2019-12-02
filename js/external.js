1b3416a2ffa6766b7d7151cf33cce431c4588bed
Noland Seigler <nolandseigler@gmail.com>
2019-12-02 11:46:58 -0600

    exercise 3 problem 2 complete. function does not work properly. it will accept any input after the first prompt

--- js/external.js
+++ js/external.js
@@ -1,39 +1,48 @@
 "use strict";
-console.log("Hello from external JavaScript.");
-alert("Welcome to my Website!");
-var userPromptInput = prompt("What is your favorite color?");
-alert(`${userPromptInput} is my favorite color too!`);
+// console.log("Hello from external JavaScript.");
+// alert("Welcome to my Website!");
+// var userPromptInput = prompt("What is your favorite color?");
+// alert(`${userPromptInput} is my favorite color too!`);
 
 // Exercise 3
 // Problem 1
-var movieQtyInput = 0
-var price = 3;
+// var price = 3;
+var inputNumber = 0
 const checkForNumber = function (inputNumber) {
     while (typeof inputNumber !== "number") {
-        inputNumber = prompt("Your input was not a number. How many days will you be renting the title?");
+        inputNumber = prompt("Your input was not a number. Please answer the question with a number.");
         return inputNumber;
     }
 }
-var littleMermaid = prompt("How many days will you be renting the Little Mermaid?");
-littleMermaid = parseInt((checkForNumber(littleMermaid)));
-var brotherBear = prompt("How many days will you be renting Brother Bear?");
-brotherBear = parseInt((checkForNumber(brotherBear)));
-var hercules = prompt("How many days will you be renting Hercules?");
-hercules = ((checkForNumber(hercules)));
-hercules = parseInt((checkForNumber(hercules)));
-var confirmPurchase = confirm("You have requested to rent Hercules for " + hercules + " days, Brother Bear for " + brotherBear + " days, and the Little Mermaid for " + littleMermaid + " days. Is this correct?");
-
-
-var rentingTotal = (littleMermaid + brotherBear + hercules) * price;
-var confirmTotal = confirm(rentingTotal + " is your total. Click ok to continue to checkout");
+// var littleMermaid = prompt("How many days will you be renting the Little Mermaid?");
+// littleMermaid = parseInt((checkForNumber(littleMermaid)));
+// var brotherBear = prompt("How many days will you be renting Brother Bear?");
+// brotherBear = parseInt((checkForNumber(brotherBear)));
+// var hercules = prompt("How many days will you be renting Hercules?");
+// hercules = ((checkForNumber(hercules)));
+// hercules = parseInt((checkForNumber(hercules)));
+// var confirmPurchase = confirm("You have requested to rent Hercules for " + hercules + " days, Brother Bear for " + brotherBear + " days, and the Little Mermaid for " + littleMermaid + " days. Is this correct?");
+//
+//
+// var rentingTotal = (littleMermaid + brotherBear + hercules) * price;
+// var confirmTotal = confirm(rentingTotal + " is your total. Click ok to continue to checkout");
 
 // Problem 2
-var google = 400;
-var amazon = 380;
-var facebook= 350;
+var googleHourlyPay = 400;
+var amazonHourlyPay = 380;
+var facebookHourlyPay = 350;
 
-var weeklyPay = (facebook * 10) + (google * 6) + (amazon * 4);
-console.log(weeklyPay);
+var weeklyGoogleHours = prompt("How many hours did you work for Google this week?");
+weeklyGoogleHours = parseInt((checkForNumber(weeklyGoogleHours)));
+var weeklyAmazonHours = prompt("How many hours did you work for Amazon this week?");
+weeklyAmazonHours = parseInt((checkForNumber(weeklyAmazonHours)));
+var weeklyFacebookHours = prompt("How many hours did you work for Facebook this week?");
+weeklyFacebookHours = parseInt((checkForNumber(weeklyFacebookHours)));
+
+var confirmWeeklyHours = confirm("Weekly hours for Google: " + weeklyGoogleHours + " . " + "Weekly hours for Amazon: " + weeklyAmazonHours + " . " + "Weekly Facebook hours: " + weeklyFacebookHours + " . " + "Click ok to confirm hours.");
+var weeklyTotalPay = (googleHourlyPay * weeklyGoogleHours) + (amazonHourlyPay * weeklyAmazonHours) + (facebookHourlyPay * weeklyFacebookHours);
+alert("Your total weekly pay was submitted as: " + weeklyTotalPay + " .");
+
 // // Problem 3
 // var classFull = true;
 // var scheduleConflict = true;
