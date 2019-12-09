be132c6ea349fa896c829cdfa369ea058ca09dac
Noland Seigler <nolandseigler@gmail.com>
2019-12-09 11:28:28 -0600

    completed but without using continue or break. practice using them.

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,15 +1,25 @@
 (function () {
 "use strict";
 //problem 1
+//didnt use break need to refactor if that if time permits
 const isOdd = function (inputFromPrompt) {
-    while (inputFromPrompt % 2 === 0) {
-        inputFromPrompt = (inputFromPrompt % 2 !== 0 ? "It is odd." : prompt("Please provide an odd number between 1 and 50."));
+    while (inputFromPrompt % 2 === 0 || inputFromPrompt < 1 || inputFromPrompt >49) {
+        inputFromPrompt = (inputFromPrompt % 2 !== 0 && inputFromPrompt >= 1 && inputFromPrompt <= 49 ? "It is odd." : parseInt(prompt("Please provide an odd number between 1 and 50.")));
     }
-    return console.log(inputFromPrompt);
+    console.log(`Number to skip is: ${inputFromPrompt}`)
+    for (var i = 1; i <= 50; i++){
+        if (i == inputFromPrompt) {
+            console.log(`Yikes! Skipping number: ${i}`);
+        } else if (i % 2 !== 0) {
+            console.log(i)
+        }
+    }
 }
 var userNumber = prompt("Please provide an odd number between 1 and 50.");
 isOdd(userNumber);
-//problem 2
+
+
+
 
 
 
