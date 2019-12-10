ac74310879a6493d00d0380dbda82ddea99e3499
Noland Seigler <nolandseigler@gmail.com>
2019-12-10 11:42:17 -0600

    exercise complete and tested

--- js/planets-array.js
+++ js/planets-array.js
@@ -28,16 +28,16 @@
 
 
     console.log('Removing "The Sun" from the beginning of the planets array.');
-    planets.shift("The Sun");
+    planets.shift();
     console.log(planets);
 
     console.log('Removing "Pluto" from the end of the planets array.');
-    planets.pop("Pluto")
+    planets.pop("Pluto");
     console.log(planets);
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
     var indexOfEarth = planets.indexOf("Earth");
-    console.log(`The index of 'Earth' is: ${indexOfEarth}`)
+    console.log(`The index of 'Earth' is: ${indexOfEarth}`);
 
     console.log("Reversing the order of the planets array.");
     planets.reverse();
