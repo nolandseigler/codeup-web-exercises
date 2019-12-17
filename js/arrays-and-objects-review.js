f55b3b5253f0cf8745df07dcd863e1897d9a719b
Noland Seigler <nolandseigler@gmail.com>
2019-12-16 20:44:50 -0600

    review complete. reference this for input of two arrays and output one array of new objects.

--- js/arrays-and-objects-review.js
+++ js/arrays-and-objects-review.js
@@ -1,5 +1,8 @@
 // 1.  Write a function, `filterNumbers()` that takes in an array of mixed data types and returns an array of only the numbers type in ascending order.
 // const filterNumbers = function (inputArr) {
+//     function sortNumber(a, b) {
+//         return a - b;
+//     }
 //     var newArr = [];
 //     var badArr = [];
 //     inputArr.forEach( element => {
@@ -7,40 +10,40 @@
 //         console.log(newArr);
 //         return newArr
 //     });
-//     var sortedArr = newArr.sort();
+//     var sortedArr = newArr.sort(sortNumber);
 //     console.log(sortedArr);
 // }
-// filterNumbers(["fred", true, 34, 31, 11, 69, 45, "32", "10"]);
+// filterNumbers(["fred", true, 34, 31, 11, 1, 2, 25, 7, 69, 45, "32", "10"]);
 //     ```js
 //         filterNumbers(["fred", true, 5, 3]) //[3, 5]
 //     ```
 //
 // 2. Write a function, `getOlder()` that takes in array of dog objects and increases the value of the age properties by 1.
 //
-const getOlder = function (inputArr) {
-    inputArr.forEach( element => {
-        element.age += 1;
-        return inputArr;
-    })
-    return inputArr
-}
-console.log(getOlder([
-    {
-        name: "Chompers",
-        breed: "Pug",
-        age: 7
-    },
-    {
-        name: "Freddy",
-        breed: "Lab",
-        age: 4
-    },
-    {
-        name: "Mr. Pig",
-        breed: "Mastif",
-        age: 10
-    }
-]));
+// const getOlder = function (inputArr) {
+//     inputArr.forEach( element => {
+//         element.age += 1;
+//         return inputArr;
+//     })
+//     return inputArr
+// }
+// console.log(getOlder([
+//     {
+//         name: "Chompers",
+//         breed: "Pug",
+//         age: 7
+//     },
+//     {
+//         name: "Freddy",
+//         breed: "Lab",
+//         age: 4
+//     },
+//     {
+//         name: "Mr. Pig",
+//         breed: "Mastif",
+//         age: 10
+//     }
+// ]));
 //     ```js
 //     //Example Input:
 //
@@ -84,7 +87,33 @@ console.log(getOlder([
 //    ```
 //
 // 3. Write a function, `washCars()` that takes in a array of car objects and sets the boolean properties of isDirty to false
-//
+// const washCars = function (inputArr) {
+//     inputArr.forEach(element => {
+//         element.isDirty = false;
+//         return inputArr;
+//     })
+//     return inputArr;
+// }
+// console.log(washCars([
+//     {
+//         make: 'Volvo',
+//         color: 'red',
+//         year: 1996,
+//         isDirty: true
+//     },
+//     {
+//         make: 'Toyota',
+//         color: 'black',
+//         year: 2004,
+//         isDirty: false
+//     },
+//     {
+//         make: 'Ford',
+//         color: 'white',
+//         year: 2007,
+//         isDirty: true
+//     }
+// ]));
 //     ```js
 //             // Example input
 //             [
@@ -134,6 +163,31 @@ console.log(getOlder([
 //     ```
 //
 // 4. Write a function, `adminList()` that takes in an array of user objects and returns a count of all admins based on the isAdmin property. Refactor to return an array of admin-only user emails. Refactor again to return an array of user objects that are admins.
+//
+// const adminList = function (inputArr) {
+//     var listOfAdmins = [];
+//     inputArr.forEach( element => {
+//         if (element.isAdmin) {
+//             listOfAdmins.push(element);
+//         }
+//     return listOfAdmins;
+//     })
+// return listOfAdmins;
+// }
+// console.log(adminList([
+//     {
+//         isAdmin: true,
+//         email: 'user1@email.com'
+//     },
+//     {
+//         isAdmin: true,
+//         email: 'user2@email.com'
+//     },
+//     {
+//         isAdmin: false,
+//         email: 'user3@email.com'
+//     }
+// ]));
 //     ```js
 //          //Example Input:
 //           [
@@ -179,6 +233,33 @@ console.log(getOlder([
 //
 // 5. Create a function, `makeSandwichObjects()` that takes in two array of strings, breads and fillings and returns an array of sandwichObjects that contain properties for bread and filling and values correspond to the same order of the two passed in arrays. Assume the two array inputs are the same length.
 //
+//REMEMBER THIS IT IS IMPORTANT
+//TODO: REMEMBER THIS AND STOP FORGETTING HOW TO TODO IT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+const makeSandwichObjects = (breadInput, fillingInput) => {
+    var sandwichObjectsArray = [];
+    for (var i = 0; i < breadInput.length; i++) {
+        var sandwich = {
+            bread: breadInput[i],
+            filling: fillingInput[i]
+        }
+        sandwichObjectsArray.push(sandwich);
+    }
+    return sandwichObjectsArray;
+}
+var breads  = [
+    "white",
+    "wheat",
+    "rhy",
+    "white"
+];
+
+var fillings = [
+    "pb&j",
+    "ham",
+    "cheese steak",
+    "tuna"
+];
+console.log(makeSandwichObjects(breads, fillings));
 //     ```js
 //         //     Example Input:
 //
