bb74964d634406b0031732fcf73659e274078782
Noland Seigler <nolandseigler@gmail.com>
2020-01-09 15:00:02 -0600

    test

--- js/promises.js
+++ js/promises.js
@@ -1,18 +1,31 @@
-import {gitHubKey} from "./keys.js";
-
+import { githubKey } from './keys.js';
 const wait = function (time) {
     return new Promise((resolve,  reject) => {
-       if (typeof time === 'number') {
-           setTimeout(() => {
-               resolve();
-           }, time);
-       }
-       else {
-           reject();
-       }
+        if (typeof time === 'number') {
+            setTimeout(() => {
+                resolve();
+            }, time);
+        }
+        else {
+            reject();
+        }
     });
 }
 
 wait(1000).then(() => console.log(`You'll see this after 1 second`));
 
-const
+console.log(githubKey);
+
+function getGithubUsernames() {
+    return fetch('https://api.github.com/users', {headers: {"Authorization": `${githubKey}`}})
+        .then(response => response.json())
+        .then(users => users.map(user => user.login));
+}
+
+// later on...
+
+getGithubUsernames().then((usernames) => {
+    usernames.forEach((username) => {
+        console.log(username)
+    });
+}).catch(error => console.error(error));
