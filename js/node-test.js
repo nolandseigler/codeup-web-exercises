6f921c15488d1d45d98142dbf1ac183c9a34f180
Noland Seigler <nolandseigler@gmail.com>
2020-01-22 11:56:44 -0600

    require using object destructuring

--- js/node-test.js
+++ js/node-test.js
@@ -1,4 +1,8 @@
 const mods = require('./node-test-modules.js');
-const {hello} = require('./node-test-modules.js');
+// const {hello} = require('./node-test-modules.js');
+const {hello, goodbye, myFunctions} = require('./node-test-modules')
 mods.hello();
 hello();
+goodbye()
+console.log(myFunctions.sum(1, 1));
+console.log(myFunctions.difference(1, 1));
