b507d567a0df25064ffac7133b7fcb1de79295cf
Noland Seigler <nolandseigler@gmail.com>
2020-01-24 10:21:21 -0600

    warmup complete

--- js/export-test.js
+++ js/export-test.js
@@ -1,11 +1,30 @@
-export const hello = () => console.log(`Hello World`);
-export const goodbye = () => console.log(`Goodbye`);
-
-export const myFunctions = {
-    sum: function(num1, num2) {
-        return num1 + num2;
+// OPTION 1
+// export const hello = () => console.log(`Hello World`);
+// export const goodbye = () => console.log(`Goodbye`);
+//
+// export const myFunctions = {
+//     sum: function(num1, num2) {
+//         return num1 + num2;
+//     },
+//     difference: function(num1, num2) {
+//         return num1 - num2;
+//     }
+// };
+//
+// OPTION 2
+const funcs = {
+    hello:  function() {
+        console.log(`Hello World`)
     },
-    difference: function(num1, num2) {
-        return num1 - num2;
+    goodbye: () => console.log(`Goodbye`),
+    myFunctions: {
+        sum: function(num1, num2) {
+            return num1 + num2;
+        },
+        difference: function(num1, num2) {
+            return num1 - num2;
+        }
     }
 };
+export default {funcs};
+//
